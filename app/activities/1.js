69d5a382342a2dd9a96b471e1088013719334338
hiroki-oist <hiroki.sawada1@oist.jp>
2025-07-17 12:07:43 +0200

    Fix route

--- app/activities/1.js
+++ app/activities/1.js
@@ -1,5 +1,5 @@
 import { useRouter } from "next/router";
-import Sidebar from "../../components/Sidebar";
+import Sidebar from "../../../components/Sidebar";
 
 export default function ActivityDetail() {
   const router = useRouter();
